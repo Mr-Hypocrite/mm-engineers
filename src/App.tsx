53c801b6417693e5e99e613c369901d12ec279b3
jatiny <jatiny@consultbop.com>
2023-05-14 21:46:26 +0530

    chore: basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { Box, Flex, Loader2 } from "@src/components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ScrollToTop, Layout } from "@src/components";
+import { Home } from "@src/pages";
 
 function App() {
     return (
-        <Flex direction="column" align="center" justify="center" size={"full"}>
-            <Box css={{ maxWidth: "300px", p: "$2" }}>
-                <img
-                    src="/mmEngineersLogo.png"
-                    className="logo"
-                    alt="Vite logo"
-                    width="100%"
-                />
-            </Box>
-            <h1>Website under construction</h1>
-            <Box css={{ size: "200px", maxWidth: "320px" }}>
-                <Loader2 />
-            </Box>
-        </Flex>
+        <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export * from "./Box";
 export * from "./Flex";
 export * from "./Loader";
+export * from "./Navbar";
+export * from "./Footer";
+export * from "./Layout";
+export * from "./ScrollToTop";
